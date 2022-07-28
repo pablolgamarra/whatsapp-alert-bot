4c96f26d0ef8094f8743c223df403db6759f6c02
Pablo Gamarra <p.gamarra.jara.1503@gmail.com>
2022-07-28 16:49:09 -0400

    Server files organized

--- sqlconnection.js
+++ sqlconnection.js
@@ -1,24 +1,19 @@
 const mysql = require('mysql');
 
-async function connection2db() {
+module.exports = {
+    async function connection2db(){
     const connection = mysql.createConnection({
         host: 'localhost',
         user: 'pablo',
         password: 'P4bl0P4$$w0rd',
         database: 'node_bot_for_whatsapp'
     })
-    try{
+    try {
         await connection.connect();
         console.log(`Connection success`);
-    }catch(error){
-        if(error){
+    } catch (error) {
+        if (error) {
             console.log(`An error was happenes when trying to connect to database. Error: ${error}`);
         }
     }
-}
-
-connection2db();
-
-module.exports = {
-    connection2db
 }
