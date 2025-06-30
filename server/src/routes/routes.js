9e067f44dc565573118cc04d10cdd91fe94d3382
pablolgamarra <p.gamarra.jara.1503@gmail.com>
2025-06-30 18:51:55 +0000

    feat: update routes in router

--- server/src/routes/routes.js
+++ server/src/routes/routes.js
@@ -1,15 +1,38 @@
 import express from 'express';
 import fs from 'node:fs';
+import path from 'path';
+import { fileURLToPath } from 'url';
 
+import sendMessage from '../bot/handlers/sendMessage.js';
+import { getQR } from '../store/qr.js';
 const Router = express.Router();
+
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
+
+//Request Logger
+Router.use((req, res, next) => {
+	console.log('Time:', Date.now());
+	console.log(`New ${req.method} request -> ${req.originalUrl}`);
+	next();
+});
+
+//Reconocer JSON's
 Router.use(express.json());
 
 Router.get('/api/qr', (req, res) => {
-	res.json({ qr: `${fs.readFileSync('QRCode.txt')}` });
+	res.json({ qr: getQR() });
 });
 
 Router.get('/api/messages', (req, res) => {
-	res.json({ messages: `${require('../config/botMessages/messages.json')}` });
+	const filePath = path.join(__dirname, '../bot/config/botMessages/messages.json');
+	try {
+		const messages = JSON.parse(fs.readFileSync(filePath, 'utf8'));
+		console.log(messages);
+		res.json({ messages });
+	} catch (err) {
+		console.error('Error leyendo messages.json', err);
+		res.status(500).json({ error: 'No se pudieron obtener los mensajes.' });
+	}
 });
 
 Router.post('/webhook/zabbix', (req, res) => {
@@ -17,7 +40,25 @@ Router.post('/webhook/zabbix', (req, res) => {
 
 	console.log('Webhook llamado -> ', data);
 
-	res.status(200).send('OK');
+	if (!data.message) {
+		return res.status(400).json({ error: 'Parámetros requeridos: to y message' });
+	}
+	try {
+		sendMessage(data.message);
+		res.status(200).json({ status: 'enviado' });
+	} catch (err) {
+		console.error('❌ Error al enviar mensaje:', err);
+		res.status(500).json({ error: 'No se pudo enviar el mensaje' });
+	}
+});
+
+// FRONTEND ENDPOINTS
+Router.get('/', (req, res) => {
+	res.sendFile(path.join(__dirname, '../public/index.html'));
+});
+
+Router.get('/messages', (req, res) => {
+	res.sendFile(path.join(__dirname, '../public/messages.html'));
 });
 
 export default Router;
