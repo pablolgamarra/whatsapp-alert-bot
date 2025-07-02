9559cc07bfdc7b8e55e390ab0f70f5e02228aa53
pablolgamarra <p.gamarra.jara.1503@gmail.com>
2025-07-02 12:17:48 +0000

    fix: update puppeteer configuration

--- server/src/bot/wp-client/getWPWebClient.js
+++ server/src/bot/wp-client/getWPWebClient.js
@@ -2,5 +2,12 @@ import webWp from 'whatsapp-web.js';
 const { Client, LocalAuth } = webWp;
 
 export default function getWAWebClient() {
-	return new Client({ authStrategy: new LocalAuth() });
+	return new Client({
+		authStrategy: new LocalAuth(),
+		puppeteer: {
+			args: ['--no-sandbox', '--disable-setuid-sandbox'],
+			ignoreHTTPSErrors: true,
+			dumpio: false,
+		},
+	});
 }
