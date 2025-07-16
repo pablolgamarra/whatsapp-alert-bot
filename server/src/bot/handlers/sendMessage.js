0cdbd1db42e1899adf64944a1e21ff5f6c3dbafb
Pablo Gamarra <78704427+pablolgamarra@users.noreply.github.com>
2025-07-16 19:49:09 +0000

    feat!: update sendMessage to receive chatId as parameter

--- server/src/bot/handlers/sendMessage.js
+++ server/src/bot/handlers/sendMessage.js
@@ -1,15 +1,17 @@
 import bot from '../index.js';
 
-//GROUP ID
-//('120363403436947168@g.us');
+export default async function sendMessage(chatId, message) {
+	if (!chatId || !message) {
+		throw Error(`Chat ID and Message are required to send a message through the bot`);
+	}
 
-export default async function sendMessage(message) {
-	const sendedMsg = await bot.sendMessage('120363403436947168@g.us', message);
+	const sendedMsg = await bot.sendMessage(chatId, message);
 
 	const logMsg = {
 		To: sendedMsg.to,
 		From: sendedMsg.from,
 		Body: sendedMsg.body,
 	};
+
 	console.log('Mensaje de Alerta Enviado por WP -> ', logMsg);
 }
