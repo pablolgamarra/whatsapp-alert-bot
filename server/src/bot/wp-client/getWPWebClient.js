08c5a4e77bdca7e79f06e28e4316d29601ce3f0c
pablolgamarra <p.gamarra.jara.1503@gmail.com>
2025-08-29 20:07:44 +0000

    feat!: update WAWebClient LocalAuth data path

--- server/src/bot/wp-client/getWPWebClient.js
+++ server/src/bot/wp-client/getWPWebClient.js
@@ -1,11 +1,17 @@
+import path from 'node:path';
+import { fileURLToPath } from 'url';
 import webWp from 'whatsapp-web.js';
 const { Client, LocalAuth } = webWp;
 
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
+
+const authPath = path.join(__dirname, '../../../auth/bot');
+
 export default function getWAWebClient() {
 	return new Client({
-		authStrategy: new LocalAuth(),
+		authStrategy: new LocalAuth({dataPath:authPath}),
 		puppeteer: {
-			args: ['--no-sandbox', '--disable-setuid-sandbox'],
+			args: ['--no-sandbox', '--disable-setuid-sandbox'], 
 			ignoreHTTPSErrors: true,
 			dumpio: false,
 		},
