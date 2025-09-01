2b1e248a484177da83d7cc9da63cb3cabf7227bb
pablolgamarra <p.gamarra.jara.1503@gmail.com>
2025-09-01 17:57:38 +0000

    feat!: update wp client cache path

--- server/src/bot/wp-client/getWPWebClient.js
+++ server/src/bot/wp-client/getWPWebClient.js
@@ -6,10 +6,12 @@ const { Client, LocalAuth } = webWp;
 const __dirname = path.dirname(fileURLToPath(import.meta.url));
 
 const authPath = path.join(__dirname, '../../../auth/bot');
+const cachePath = path.join(__dirname, '../../../cache/bot');
 
 export default function getWAWebClient() {
 	return new Client({
 		authStrategy: new LocalAuth({dataPath:authPath}),
+        webVersionCache: {type: 'local', path: cachePath},
 		puppeteer: {
 			args: ['--no-sandbox', '--disable-setuid-sandbox'], 
 			ignoreHTTPSErrors: true,
