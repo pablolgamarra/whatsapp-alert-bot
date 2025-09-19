0ae59cc72bcb0009fc95325998f7f40df49ae3fd
Pablo Gamarra <78704427+pablolgamarra@users.noreply.github.com>
2025-09-19 13:47:41 +0000

    fix!: update qr count query route

--- src/routes/api/v2/qr/qr.js
+++ src/routes/api/v2/qr/qr.js
@@ -4,6 +4,6 @@ import { qrController } from '../../../../controllers/qr/qrController.js';
 const router = Router();
 
 router.get('/', qrController.getQrCode);
-router.get('-count', qrController.getQrCode);
+router.get('/count', qrController.getQRCount);
 
 export default router;
