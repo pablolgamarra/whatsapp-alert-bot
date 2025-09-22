bc2727c089d1de18bf605d119259e44df787dae0
Pablo Gamarra <78704427+pablolgamarra@users.noreply.github.com>
2025-09-22 14:44:54 +0000

    feat: update WwebClient to be able to use environment variables

--- src/bot/wp-client/getWPWebClient.js
+++ src/bot/wp-client/getWPWebClient.js
@@ -5,14 +5,14 @@ const { Client, LocalAuth } = webWp;
 
 const __dirname = path.dirname(fileURLToPath(import.meta.url));
 
-const authPath = path.join(__dirname, '../../../auth/bot');
-const cachePath = path.join(__dirname, '../../../cache/bot');
+const authPath = process.env.AUTH_PATH || path.join(__dirname, '../../../auth/bot');
+const cachePath = process.env.CACHE_PATH || path.join(__dirname, '../../../cache/bot');
 
 export default function getWAWebClient() {
 	return new Client({
-		authStrategy: new LocalAuth({clientId: 'botcito', dataPath:authPath, rmMaxRetries: 3}),
+		authStrategy: new LocalAuth({clientId: 'Whatsapp-Bot', dataPath:authPath, rmMaxRetries: 3}),
 		webVersionCache: {path: cachePath},
-        deviceName: 'Wp Bot',
+        deviceName: 'Bot Runner',
 		puppeteer: {
 			args: ['--no-sandbox', '--disable-setuid-sandbox'],
 			ignoreHTTPSErrors: true,
